bee0e47b77c6af15c9ed2499bfa6d985cf813094
haniabukhaitcode <haniabukhait1@gmail.com>
2019-08-27 08:55:36 +0300

    rest php

--- Models/post.php
+++ Models/post.php
@@ -1,35 +1,161 @@
-<?php
-//refer to 
-// https://t4tutorials.com/left-join-in-php-source-code/ 
-class Post {
-
-    //DB stuff
+<?php 
+  class Post {
+    // DB stuff
     private $conn;
-    private $table = users;
-    //Post Properties
+    private $table = 'posts';
+
+    // Post Properties
     public $id;
-    public $first_name;
-    public $skill_id;
-    public $skill_name;
+    public $category_id;
+    public $category_name;
     public $title;
     public $body;
+    public $author;
     public $created_at;
 
-    //Constructor class equivalent
+    // Constructor with DB
     public function __construct($db) {
-        $this->$conn = $db;
+      $this->conn = $db;
     }
+
     // Get Posts
     public function read() {
-        $query = "SELECT c.name as skill_name, p.id, p.category_id, p.title, p.body, p.created_at users"
-                . "FROM $this->table p LEFT JOIN skills c ON p.skill_id = c.id ORDER BY p.created_at DESC";
+      // Create query
+      $query = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
+                                FROM ' . $this->table . ' p
+                                LEFT JOIN
+                                  categories c ON p.category_id = c.id
+                                ORDER BY
+                                  p.created_at DESC';
+      
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
 
+      // Execute query
+      $stmt->execute();
 
-        //prepare statement
-        $statement = $this->conn->prepare($query);
-        //execute statement
-        $statement->execute();
-        return statement();
+      return $stmt;
     }
 
-}
+    // Get Single Post
+    public function read_single() {
+          // Create query
+          $query = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
+                                    FROM ' . $this->table . ' p
+                                    LEFT JOIN
+                                      categories c ON p.category_id = c.id
+                                    WHERE
+                                      p.id = ?
+                                    LIMIT 0,1';
+
+          // Prepare statement
+          $stmt = $this->conn->prepare($query);
+
+          // Bind ID
+          $stmt->bindParam(1, $this->id);
+
+          // Execute query
+          $stmt->execute();
+
+          $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+          // Set properties
+          $this->title = $row['title'];
+          $this->body = $row['body'];
+          $this->author = $row['author'];
+          $this->category_id = $row['category_id'];
+          $this->category_name = $row['category_name'];
+    }
+
+    // Create Post
+    public function create() {
+          // Create query
+          $query = 'INSERT INTO ' . $this->table . ' SET title = :title, body = :body, author = :author, category_id = :category_id';
+
+          // Prepare statement
+          $stmt = $this->conn->prepare($query);
+
+          // Clean data
+          $this->title = htmlspecialchars(strip_tags($this->title));
+          $this->body = htmlspecialchars(strip_tags($this->body));
+          $this->author = htmlspecialchars(strip_tags($this->author));
+          $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+
+          // Bind data
+          $stmt->bindParam(':title', $this->title);
+          $stmt->bindParam(':body', $this->body);
+          $stmt->bindParam(':author', $this->author);
+          $stmt->bindParam(':category_id', $this->category_id);
+
+          // Execute query
+          if($stmt->execute()) {
+            return true;
+      }
+
+      // Print error if something goes wrong
+      printf("Error: %s.\n", $stmt->error);
+
+      return false;
+    }
+
+    // Update Post
+    public function update() {
+          // Create query
+          $query = 'UPDATE ' . $this->table . '
+                                SET title = :title, body = :body, author = :author, category_id = :category_id
+                                WHERE id = :id';
+
+          // Prepare statement
+          $stmt = $this->conn->prepare($query);
+
+          // Clean data
+          $this->title = htmlspecialchars(strip_tags($this->title));
+          $this->body = htmlspecialchars(strip_tags($this->body));
+          $this->author = htmlspecialchars(strip_tags($this->author));
+          $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+          $this->id = htmlspecialchars(strip_tags($this->id));
+
+          // Bind data
+          $stmt->bindParam(':title', $this->title);
+          $stmt->bindParam(':body', $this->body);
+          $stmt->bindParam(':author', $this->author);
+          $stmt->bindParam(':category_id', $this->category_id);
+          $stmt->bindParam(':id', $this->id);
+
+          // Execute query
+          if($stmt->execute()) {
+            return true;
+          }
+
+          // Print error if something goes wrong
+          printf("Error: %s.\n", $stmt->error);
+
+          return false;
+    }
+
+    // Delete Post
+    public function delete() {
+          // Create query
+          $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id';
+
+          // Prepare statement
+          $stmt = $this->conn->prepare($query);
+
+          // Clean data
+          $this->id = htmlspecialchars(strip_tags($this->id));
+
+          // Bind data
+          $stmt->bindParam(':id', $this->id);
+
+          // Execute query
+          if($stmt->execute()) {
+            return true;
+          }
+
+          // Print error if something goes wrong
+          printf("Error: %s.\n", $stmt->error);
+
+          return false;
+    }
+    
+  }
--- config/database.php
+++ config/database.php
@@ -1,36 +1,23 @@
-<?php
-
-// Following Link 
-// https://www.w3resource.com/php/pdo/php-pdo.php#DTPDO
-class Database {
-
-//db params equivalent to java private classes
-    private $host = "localhost";
-    private $db_name = "PHPSQL";
-    private $username = "root";
-    private $password = "root";
+<?php 
+  class Database {
+    // DB Params
+    private $host = 'localhost';
+    private $db_name = 'myblog';
+    private $username = 'root';
+    private $password = '';
     private $conn;
 
-//db connect
+    // DB Connect
     public function connect() {
-        //equivalent to Javascript this.foo
-        $this->conn = null;
+      $this->conn = null;
 
+      try { 
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch(PDOException $e) {
+        echo 'Connection Error: ' . $e->getMessage();
+      }
 
-//php data object similar to JavaScript error catcher but without Async/await 
-        try {
-            $this->conn = new PDO("mysql:host= " . $this->host . ";dbname " . $this->db_name, $this->username, $this->password);
-            //Does the attribute refere to keys or values of private functions? (Question);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) { //PHP Data Object Exception
-            echo 'Connection Error: ' . $e->getMessage();
-        }
-        return $this->$conn;
+      return $this->conn;
     }
-
-}
-
-//PDOException give the error with root path? (Question)
-
-// for PDO EXCEPTIONS and ATTR refer too
-// https://www.php.net/manual/en/pdo.error-handling.php
+  }
